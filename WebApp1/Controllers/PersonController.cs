be48180a67d886e3b159d7fa2c2013f0d6fa78c0
max.brandstetter <max.brandstetter@W209-7010-13>
2016-10-01 09:35:06 -0700

    Add project files.

---
+++ WebApp1/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Old code, before model
+            ViewBag.Name = "Max Brandstetter";
+            ViewBag.DOB = "November 19, 1995";
+            ViewBag.Address = "1234 Something Street, Oregon City, OR";
+
+            // Same as above if but using the Person model, not usable without database?
+            Person person = new Models.Person();
+            person.Name = "Max Brandstetter";
+            person.DOB = "November 19, 1995";
+            person.Address = "1234 Something Street, Oregon City, OR";
+
+            return View();
+        }
+
+    }
+}
---
+++ WebApp1/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp1.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string DOB { get; set; }
+        public string Address { get; set; }
+    }
+}
